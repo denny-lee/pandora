c1f5f7d8ead7a136f7cf3076a679512a55578704
denny-lee <zhuimeng12321@gmail.com>
2018-06-23 23:35:24 +0800

    fix known bugs

--- src/main/java/study/lab/algorithm/RubicsCube.java
+++ src/main/java/study/lab/algorithm/RubicsCube.java
@@ -1,24 +1,32 @@
 package study.lab.algorithm;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class RubicsCube {
 
     private int[] method;
     private int tailIndex;
+    private Map<String, Object> answers = new LinkedHashMap<String, Object>(2, 0.75f, true);
 
     private Block[] cube;
 
     RubicsCube() {
         cube = new Block[]{
-                new Block(0, Color.BLUE.getValue(), Color.BLACK.getValue(), Color.RED.getValue()),
+                new Block(0, Color.BLUE.getValue(), Color.WHITE.getValue(), Color.RED.getValue()),
                 new Block(1, Color.BLUE.getValue(), Color.YELLOW.getValue(), Color.RED.getValue()),
-                new Block(2, Color.BLACK.getValue(), Color.GREEN.getValue(), Color.ORANGE.getValue()),
-                new Block(3, Color.BLACK.getValue(), Color.BLUE.getValue(), Color.ORANGE.getValue()),
-                new Block(4, Color.GREEN.getValue(), Color.BLACK.getValue(), Color.RED.getValue()),
+                new Block(2, Color.WHITE.getValue(), Color.GREEN.getValue(), Color.ORANGE.getValue()),
+                new Block(3, Color.WHITE.getValue(), Color.BLUE.getValue(), Color.ORANGE.getValue()),
+                new Block(4, Color.GREEN.getValue(), Color.WHITE.getValue(), Color.RED.getValue()),
                 new Block(5, Color.GREEN.getValue(), Color.YELLOW.getValue(), Color.RED.getValue()),
                 new Block(6, Color.YELLOW.getValue(), Color.GREEN.getValue(), Color.ORANGE.getValue()),
                 new Block(7, Color.YELLOW.getValue(), Color.BLUE.getValue(), Color.ORANGE.getValue()),
         };
-        method = new int[2];
+        System.out.println("-------init-------");
+        printStatus();
+        System.out.println("------------------");
+
+        method = new int[7];
         for (int i = 0; i<method.length; i++) {
             method[i] = -1;
         }
@@ -26,21 +34,34 @@ public class RubicsCube {
 
     public static void main(String[] args) {
         RubicsCube rc = new RubicsCube();
+//        rc.turn(0);
+//        rc.turn(1);
+//        rc.turn(2);
+//        rc.printStatus();
         rc.solve(-1);
+        rc.printAnswer();
+    }
+
+    private void printAnswer() {
+        for (String s : answers.keySet()) {
+            System.out.println(s);
+        }
     }
 
     public void solve(int lastMethod) {
         if (test()) {
             printMethod();
         } else {
-            if (method[1] != -1) {
+            if (method[6] != -1) {
                 return;
             }
             for (int i = 0; i < 18; i++) {
-                if (lastMethod == i) {
+                if (lastMethod == i || Math.abs(lastMethod - i)%6 == 0) {
                     continue;
                 }
                 turn(i);
+
+//                printStatus();
                 method[tailIndex] = i;
                 tailIndex++;
                 solve(i);
@@ -49,6 +70,37 @@ public class RubicsCube {
                 restore(i);
             }
         }
+    }
+
+    private void printStatus() {
+        System.out.println("   "+getLabel(cube[4].getColor()[0])+getLabel(cube[5].getColor()[0])+"   ");
+        System.out.println("   "+getLabel(cube[6].getColor()[0])+getLabel(cube[7].getColor()[0])+"   ");
+
+        System.out.println(getLabel(cube[4].getColor()[1])
+                +getLabel(cube[6].getColor()[1])+" "
+                +getLabel(cube[6].getColor()[2])
+                +getLabel(cube[7].getColor()[2])+" "
+                +getLabel(cube[7].getColor()[1])
+                +getLabel(cube[5].getColor()[1])
+        );
+        System.out.println(getLabel(cube[0].getColor()[1])
+                +getLabel(cube[2].getColor()[1])+" "
+                +getLabel(cube[2].getColor()[2])
+                +getLabel(cube[3].getColor()[2])+" "
+                +getLabel(cube[3].getColor()[1])
+                +getLabel(cube[1].getColor()[1])
+        );
+
+        System.out.println("   "+getLabel(cube[2].getColor()[0])+getLabel(cube[3].getColor()[0])+"   ");
+        System.out.println("   "+getLabel(cube[0].getColor()[0])+getLabel(cube[1].getColor()[0])+"   ");
+
+        System.out.println("   "+getLabel(cube[0].getColor()[2])+getLabel(cube[1].getColor()[2])+"   ");
+        System.out.println("   "+getLabel(cube[4].getColor()[2])+getLabel(cube[5].getColor()[2])+"   ");
+        System.out.println("----------");
+    }
+
+    private String getLabel(int i) {
+        return Color.getByValue(i).name().substring(0, 1);
     }
 
     private void restore(int i) {
@@ -67,16 +119,33 @@ public class RubicsCube {
                 cube[6].turn(1);
                 cube[7].turn(1);
             }
+            Block tmp = cube[2];
+            cube[2] = cube[3];
+            cube[3] = cube[7];
+            cube[7] = cube[6];
+            cube[6] = tmp;
         } else if (i == M.D.getValue()) {
             cube[0].turn(1);
             cube[1].turn(1);
             cube[4].turn(1);
             cube[5].turn(1);
+
+            Block tmp = cube[0];
+            cube[0] = cube[4];
+            cube[4] = cube[5];
+            cube[5] = cube[1];
+            cube[1] = tmp;
         } else if (i == M.L.getValue()) {
             cube[0].turn(0);
             cube[2].turn(0);
             cube[4].turn(0);
             cube[6].turn(0);
+
+            Block tmp = cube[0];
+            cube[0] = cube[2];
+            cube[2] = cube[6];
+            cube[6] = cube[4];
+            cube[4] = tmp;
         } else if (i == M.R.getValue()) {
             for (int j = 0; j < 3; j++) {
                 cube[1].turn(0);
@@ -84,11 +153,23 @@ public class RubicsCube {
                 cube[5].turn(0);
                 cube[7].turn(0);
             }
+
+            Block tmp = cube[1];
+            cube[1] = cube[5];
+            cube[5] = cube[7];
+            cube[7] = cube[3];
+            cube[3] = tmp;
         } else if (i == M.F.getValue()) {
             cube[0].turn(2);
             cube[1].turn(2);
             cube[2].turn(2);
             cube[3].turn(2);
+
+            Block tmp = cube[0];
+            cube[0] = cube[1];
+            cube[1] = cube[3];
+            cube[3] = cube[2];
+            cube[2] = tmp;
         } else if (i == M.B.getValue()) {
             for (int j = 0; j < 3; j++) {
                 cube[4].turn(2);
@@ -96,11 +177,23 @@ public class RubicsCube {
                 cube[6].turn(2);
                 cube[7].turn(2);
             }
+
+            Block tmp = cube[4];
+            cube[4] = cube[6];
+            cube[6] = cube[7];
+            cube[7] = cube[5];
+            cube[5] = tmp;
         } else if (i == M.U1.getValue()) {
             cube[2].turn(1);
             cube[3].turn(1);
             cube[6].turn(1);
             cube[7].turn(1);
+
+            Block tmp = cube[2];
+            cube[2] = cube[6];
+            cube[6] = cube[7];
+            cube[7] = cube[3];
+            cube[3] = tmp;
         } else if (i == M.D1.getValue()) {
             for (int j = 0; j < 3; j++) {
                 cube[0].turn(1);
@@ -108,6 +201,12 @@ public class RubicsCube {
                 cube[4].turn(1);
                 cube[5].turn(1);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[1];
+            cube[1] = cube[5];
+            cube[5] = cube[4];
+            cube[4] = tmp;
         } else if (i == M.L1.getValue()) {
             for (int j = 0; j < 3; j++) {
                 cube[0].turn(0);
@@ -115,11 +214,23 @@ public class RubicsCube {
                 cube[4].turn(0);
                 cube[6].turn(0);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[4];
+            cube[4] = cube[6];
+            cube[6] = cube[2];
+            cube[2] = tmp;
         } else if (i == M.R1.getValue()) {
             cube[1].turn(0);
             cube[3].turn(0);
             cube[5].turn(0);
             cube[7].turn(0);
+
+            Block tmp = cube[1];
+            cube[1] = cube[3];
+            cube[3] = cube[7];
+            cube[7] = cube[5];
+            cube[5] = tmp;
         } else if (i == M.F1.getValue()) {
             for (int j = 0; j < 3; j++) {
                 cube[0].turn(2);
@@ -127,11 +238,23 @@ public class RubicsCube {
                 cube[2].turn(2);
                 cube[3].turn(2);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[2];
+            cube[2] = cube[3];
+            cube[3] = cube[1];
+            cube[1] = tmp;
         } else if (i == M.B1.getValue()) {
             cube[4].turn(2);
             cube[5].turn(2);
             cube[6].turn(2);
             cube[7].turn(2);
+
+            Block tmp = cube[4];
+            cube[4] = cube[5];
+            cube[5] = cube[7];
+            cube[7] = cube[6];
+            cube[6] = tmp;
         } else if (i == M.U2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[2].turn(1);
@@ -139,6 +262,13 @@ public class RubicsCube {
                 cube[6].turn(1);
                 cube[7].turn(1);
             }
+
+            Block tmp = cube[2];
+            cube[2] = cube[7];
+            cube[7] = tmp;
+            tmp = cube[3];
+            cube[3] = cube[6];
+            cube[6] = tmp;
         } else if (i == M.D2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[0].turn(1);
@@ -146,6 +276,13 @@ public class RubicsCube {
                 cube[4].turn(1);
                 cube[5].turn(1);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[5];
+            cube[5] = tmp;
+            tmp = cube[1];
+            cube[1] = cube[4];
+            cube[4] = tmp;
         } else if (i == M.L2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[0].turn(0);
@@ -153,6 +290,13 @@ public class RubicsCube {
                 cube[4].turn(0);
                 cube[6].turn(0);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[6];
+            cube[6] = tmp;
+            tmp = cube[2];
+            cube[2] = cube[4];
+            cube[4] = tmp;
         } else if (i == M.R2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[1].turn(0);
@@ -160,6 +304,13 @@ public class RubicsCube {
                 cube[5].turn(0);
                 cube[7].turn(0);
             }
+
+            Block tmp = cube[1];
+            cube[1] = cube[7];
+            cube[7] = tmp;
+            tmp = cube[3];
+            cube[3] = cube[5];
+            cube[5] = tmp;
         } else if (i == M.F2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[0].turn(2);
@@ -167,6 +318,13 @@ public class RubicsCube {
                 cube[2].turn(2);
                 cube[3].turn(2);
             }
+
+            Block tmp = cube[0];
+            cube[0] = cube[3];
+            cube[3] = tmp;
+            tmp = cube[1];
+            cube[1] = cube[2];
+            cube[2] = tmp;
         } else if (i == M.B2.getValue()) {
             for (int j = 0; j < 2; j++) {
                 cube[4].turn(2);
@@ -174,18 +332,36 @@ public class RubicsCube {
                 cube[6].turn(2);
                 cube[7].turn(2);
             }
+
+            Block tmp = cube[4];
+            cube[4] = cube[7];
+            cube[7] = tmp;
+            tmp = cube[5];
+            cube[5] = cube[6];
+            cube[6] = tmp;
         }
     }
 
     private void printMethod() {
-        System.out.println("Done!");
+//        System.out.println("Done!");
+        StringBuilder sb = new StringBuilder();
         for (int i : method) {
             if (i == -1) {
                 break;
             }
-            System.out.print(M.getByValue(i).name() + ",");
+            sb.append(",").append(M.getByValue(i).name());
         }
-        System.out.println();
+        String key = sb.toString().substring(1);
+        if (answers.isEmpty()) {
+            answers.put(key, 1);
+        } else {
+            for (String k : answers.keySet()) {
+                if (key.length() < k.length()) {
+                    answers.put(key, 1);
+                }
+            }
+        }
+
     }
 
     public boolean test() {
@@ -218,18 +394,18 @@ public class RubicsCube {
 
         public void turn(int axis) {
             if (axis == 0) {
-                changeValue(color, 0, 2);
+                changeValue(0, 2);
             } else if (axis == 1) {
-                changeValue(color, 0, 1);
+                changeValue(0, 1);
             } else if (axis == 2) {
-                changeValue(color, 1, 2);
+                changeValue(1, 2);
             }
         }
 
-        private void changeValue(int[] originColor, int i, int i1) {
-            int tmp = originColor[i];
-            originColor[i] = originColor[i1];
-            originColor[i1] = tmp;
+        private void changeValue(int i, int i1) {
+            int tmp = color[i];
+            color[i] = color[i1];
+            color[i1] = tmp;
         }
 
         public int getPos() {
@@ -250,7 +426,7 @@ public class RubicsCube {
     }
 
     enum Color {
-        BLACK(0), YELLOW(1), RED(2), GREEN(3), BLUE(4), ORANGE(5);
+        WHITE(0), YELLOW(1), RED(2), GREEN(3), BLUE(4), ORANGE(5);
         private int value;
 
         Color(int value) {
@@ -259,6 +435,15 @@ public class RubicsCube {
 
         public int getValue() {
             return value;
+        }
+
+        public static Color getByValue(int value) {
+            for (Color c : values()) {
+                if (c.value == value) {
+                    return c;
+                }
+            }
+            return null;
         }
     }
 
